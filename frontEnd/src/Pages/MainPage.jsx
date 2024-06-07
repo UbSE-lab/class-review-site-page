c026467b7d6383e046e08c822934d46d09ba2d09
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-06-07 19:08:54 +0900

    feat: Version 1.0 push

---
+++ frontEnd/src/Pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "../Components/Main";
+
+function MainPage(props) {
+  return (
+    <>
+      <Main></Main>
+    </>
+  );
+}
+
+export default MainPage;
